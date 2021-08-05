0f4a166257cb46b9cce2d66ce7ffbfdc81faf649
Carwin Young <cy@carw.in>
2021-08-04 19:21:17 -0500

    feat(group): Add a new group tag for categorization.

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const whenTag = require('./lib/when');
 const givenTag = require('./lib/given');
 const thenTag = require('./lib/then');
 const needsTag = require('./lib/needs');
+const groupTag = require('./lib/group');
 
 exports.defineTags = function(dictionary) {
-    dictionary.defineTag(stepTag.name, stepTag.options);
-    dictionary.defineTag(stepAliasTag.name, stepAliasTag.options);
-    dictionary.defineTag(whenTag.name, whenTag.options);
-    dictionary.defineTag(givenTag.name, givenTag.options);
-    dictionary.defineTag(thenTag.name, thenTag.options);
-    dictionary.defineTag(needsTag.name, needsTag.options);
+  dictionary.defineTag(stepTag.name, stepTag.options);
+  dictionary.defineTag(stepAliasTag.name, stepAliasTag.options);
+  dictionary.defineTag(whenTag.name, whenTag.options);
+  dictionary.defineTag(givenTag.name, givenTag.options);
+  dictionary.defineTag(thenTag.name, thenTag.options);
+  dictionary.defineTag(needsTag.name, needsTag.options);
+  dictionary.defineTag(groupTag.name, groupTag.options);
 };
